fed3fc63be4f95858a69f8c04167de9f9718c3a0
Olga Kargan <olgakargan72@gmail.com>
2022-10-01 16:22:21 +0400

    первый

---
+++ src/test/java/pro/sky/skypro2/demo/Skypro28DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skypro28DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
